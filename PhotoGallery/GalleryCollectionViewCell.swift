75633a43f728f62f8866fb5b76ec133d33ffeec5
Andrey Kim <akart.dev@gmail.com>
2021-07-20 05:26:49 +0600

    Cell configuration

---
+++ PhotoGallery/GalleryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  GalleryCollectionViewCell.swift
+//  PhotoGallery
+//
+//  Created by Andrey Kim on 20.07.2021.
+//
+
+import UIKit
+
+class GalleryCollectionViewCell: UICollectionViewCell {
+    
+}
